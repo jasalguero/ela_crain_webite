9f3748b254956dcba50e44eea35923477a4f346c
jasalguero <jasalguero@gmail.com>
2017-08-05 15:55:19 +0200

    added first draft for Home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import '../styles/Home.css';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
-    render() {
-        return (
-            <div class="home">
-                <div class="left panel">
-                    <span>Writing</span>
-                </div>
-                <div class="right panel">
-                    <span>Coaching</span>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="home">
+        <div className="left panel">
+          <div className="label">
+            <Link to="/writing">Writing</Link>
+          </div>
+        </div>
+
+        <div className="right panel">
+          <div className="label">
+            <Link to="/coaching">Coaching</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
