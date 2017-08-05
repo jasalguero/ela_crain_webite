ed7e0fc4a5333e6eb3fbd3783c2418d021a460a9
jasalguero <jasalguero@gmail.com>
2017-08-05 15:24:35 +0200

    more structure

---
+++ src/routes/Writing.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Writing extends Component {
+    render() {
+        return (
+            <h1>Writing</h1>
+        )
+    }
+}
+
+export default Writing
---
+++ src/utils/WritingAPI.js
@@ -0,0 +1,19 @@
+
+//const api = "https://reactnd-books-api.udacity.com"
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+//TODO: Implement API for Wordpress Writing posts
+
+// export const getPost = (postId) =>
+//   fetch(`${api}/posts/${postId}`, { headers })
+//     .then(res => res.json())
+//     .then(data => data.book)
+
+// export const getAllPosts = () =>
+//   fetch(`${api}/posts`, { headers })
+//     .then(res => res.json())
+//     .then(data => data.books)
