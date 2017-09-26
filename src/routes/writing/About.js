f43ed1694b701fc002dcb9297d148acf8f0bef08
jasalguero <jasalguero@gmail.com>
2017-09-26 17:45:26 +0530

    Update About styles to match latest design

--- src/routes/writing/About.js
+++ src/routes/writing/About.js
@@ -36,56 +36,30 @@ class AboutRoute extends Component {
             {/* REVIEWS */}
             <section>
               <h1>Reviews</h1>
-              <p>
-                Here’s what some people said about my writing. I hope you can
-                relate to what you read. Don’t be shy and hit me up with your
-                feedback!
-              </p>
               <span className="review">
-                “Ela is a forward-thinking author whose experimental style and
-                content challenges conventional views of the world around us.
-                Her characters are fantastic yet highly accessible, and through
-                them and the universes she creates she walks a tightrope between
-                madness and sanity, present and future, and right and wrong –
-                always questioning, and never short of theories to help provide
-                answers. ~ Cheryl
+                “Ela Crain is a forward-thinking author whose experimental style
+                and content challenges conventional views of the world around
+                us.”
+                <span className="author">
+                  Cheryl Whittaker, Editor, Belgium
+                </span>
               </span>
               <span className="review">
-                “Ela Crain specializes in the unseeable. Labelled generically as
-                “dark” and “surreal,” she transcends familiar classifications
-                with slice-of-life tales otherwise found in the strangest of
-                nightmares. With an unmatched sense of realism, she haunts us
-                with ominously possible futures, uncanny psychological
-                distortions, and visions too dreamlike to be real, too familiar
-                to be false. Her trademark is a stunning attention to detail,
-                which shines most luminously in casual dialogues and dramatic
-                conclusions. As she engages us in thoroughly convincing
-                scenarios, we are gradually paralyzed by a combined sense of
-                wonder and fear—and we realize it only after our emotion has
-                been fully invested. In the end, the effect is
-                manifold—profound, scarring, bizarre, charming, unique,
-                paradoxical—resulting in what might best be labelled as
-                “traumatic euphoria.” ~ Henry Sane
+                “Ela haunts us with ominously possible futures, uncanny
+                psychological distortions, and visions too dreamlike to be real,
+                too familiar to be false.”
+                <span className="author">
+                  Henry Sane, Editor, North Carolina
+                </span>
               </span>
               <span className="review">
-                “It is the young writer, the talented, ambitious, and creatively
-                driven writer, that thinks this writing game is merely about
-                words — their music, poetry, and association with one another.
-                It is the wise writer, the experienced, who learns that it is
-                more about ideas. It is the ability to distill a complex notion
-                down to a few simple words that anyone, educated or ignorant,
-                old or young, can understand. Only… no, it isn’t just about
-                understanding, but conveying the feeling of understanding.
-                Channeling that feeling of truth into the world through words,
-                that’s what writing really is. The young poet or the wise
-                distiller of truth, most of us get to be one or the other — if
-                we’re lucky — but Ela Crain is one of the rare ones that got to
-                be both. Her words are trimmed down to the subatomic, to the
-                essence of her persona, and yet they are as elegant and familiar
-                as rain on flowers. When you read her work, as I have, the hope
-                is to draw just close enough to steal some of her without
-                getting so close you become envious.” ~Trevor D. Richardson,
-                Author of American Bastard
+                “The young poet or the wise distiller of truth, most of us get
+                to be one or the other -- if we're lucky -- but Ela Crain is one
+                of the rare ones that got to be both.”
+                <span className="author">
+                  Trevor D. Richardson, Author of _American Bastards_,
+                  California
+                </span>
               </span>
             </section>
           </div>
