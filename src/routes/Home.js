03f4fc0dbeb4e0cfd998e0e5bb06224b687ed2ce
jasalguero <jasalguero@gmail.com>
2017-09-27 18:33:36 +0530

    Updated main page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,12 +9,18 @@ class Home extends Component {
         <div className="left panel">
           <div className="label">
             <Link to="/writing/posts">Writing</Link>
+            <div className="sub">
+              Explore a collection of my short stories and poems.
+            </div>
           </div>
         </div>
 
         <div className="right panel">
           <div className="label">
             <Link to="/coaching">Coaching</Link>
+            <div className="sub">
+              Discover my latest blog posts and join my workshops.
+            </div>
           </div>
         </div>
       </div>
