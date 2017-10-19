a50f57e588799f712f1cdad52df69637b070c59e
Enrico Scherlies <e.scherlies@me.com>
2017-10-19 18:10:53 +0200

    added scroll through singlePostPreviews

--- src/components/coaching/SingleList.js
+++ src/components/coaching/SingleList.js
@@ -3,7 +3,38 @@ import SinglePost from './SinglePost';
 
 class SingleList extends Component {
   state = {
-    currentPostIndex: 0
+    currentPostIndex: 0,
+    scrollable: true
+  };
+
+  componentDidMount() {
+    window.addEventListener('wheel', this.handleWheel);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('wheel', this.handleWheel);
+  }
+
+  handleWheel = event => {
+    const threshold = 1;
+    const down = event.deltaY > threshold;
+    const up = event.deltaY < -threshold;
+    console.log(event.movementY, event.deltaY);
+
+    if (this.state.scrollable) {
+      if (up || down) {
+        let currentPostIndex = this.state.currentPostIndex;
+        if (up && currentPostIndex === 0) return;
+        if (down && currentPostIndex >= this.props.items.length - 1) return;
+        this.setState({
+          currentPostIndex: down ? ++currentPostIndex : --currentPostIndex,
+          scrollable: false
+        });
+        setTimeout(() => {
+          this.setState({ scrollable: true });
+        }, 1000);
+      }
+    }
   };
 
   render() {
