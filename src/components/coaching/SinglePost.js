d4cd074552f3c81dbdcaba60c0314b826e1e31aa
Enrico Scherlies <e.scherlies@me.com>
2017-10-19 18:31:00 +0200

    changing single post coaching style  to single post writing stlye

--- src/components/coaching/SinglePost.js
+++ src/components/coaching/SinglePost.js
@@ -10,15 +10,19 @@ class SinglePost extends Component {
     const labels = post.fields && post.fields.labels;
 
     return (
-      <div
-        className="single-post coaching"
-        style={{
-          background: `url("${image_url}") no-repeat center center fixed`,
-          backgroundSize: 'cover'
-        }}
-      >
+      <div className="single-post">
         <Header showLayoutIcons={true} onViewModeChange={this.changeViewMode} />
         <div className="labels">{labels}</div>
+        <div className="image-collage">
+          <div className="image-wrapper">
+            <img
+              src={image_url}
+              alt={`${post.title}-top`}
+              className=""
+              style={{ width: '100%' }}
+            />
+          </div>
+        </div>
         <div className="title">{post.title}</div>
         <Link className="read-more-link" to={`/coaching/posts/${post.id}`}>
           Read More
