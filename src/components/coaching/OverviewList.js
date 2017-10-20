f2125185d7e2ef1225f1c1ca850faf4f1b35e9d0
Enrico Scherlies <e.scherlies@me.com>
2017-10-20 19:58:59 +0200

    fixed missing css in overviewList

--- src/components/coaching/OverviewList.js
+++ src/components/coaching/OverviewList.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../../styles/OverviewPostList.css';
+import '../../styles/coaching/OverviewPostList.css';
 
 class PostList extends Component {
   render() {
     const posts = this.props.items;
 
     return (
-      <ul className="overview-post-list">
+      <ul className="overview-post-list coaching">
         {posts.map(post => (
           <li key={post.title} className="post">
-            <Link to={`/writing/posts/${post.id}`}>
+            <Link to={`/coaching/posts/${post.id}`}>
               <div className="title">{post.title}</div>
               {/* <div className="info">Short Story, 2016</div> */}
             </Link>
