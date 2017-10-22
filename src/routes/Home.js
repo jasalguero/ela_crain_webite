da64fa76bb841c7dcf9ea258c18da700f1bc222c
Enrico Scherlies <e.scherlies@me.com>
2017-10-22 18:44:25 +0200

    content and style changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,7 +11,8 @@ class Home extends Component {
           <div className="label">
             <Link to="/writing/posts">Writing</Link>
             <div className="sub">
-              Explore a collection of my short stories and poems.
+              I refuse to memorize while I can discover. <br />
+              I refuse to fit in while I can inspire.
             </div>
           </div>
         </div>
@@ -20,7 +21,8 @@ class Home extends Component {
           <div className="label">
             <Link to="/coaching">Coaching</Link>
             <div className="sub">
-              Discover my latest blog posts and join my workshops.
+              I create freedom of self-expression. <br />
+              I create unity in diversity.
             </div>
           </div>
         </div>
