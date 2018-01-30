4ae01d7dda30ff1bda23c7c65d4b39d7895a4ba6
Enrico Scherlies <e.scherlies@me.com>
2018-01-30 22:28:16 +0100

    [wip] testimonials

--- src/components/coaching/Testimonials.js
+++ src/components/coaching/Testimonials.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import '../../styles/coaching/Testimonials.css';
 import _ from 'lodash';
 
-const WorkshopList = props => {
-  return (
-    <div className="testimonials">
-      <div className="header">What people say</div>
-      <div className="items">
-        {_.map(props.reviews, (review, index) => {
-          return (
-            <div key={index} className="item">
-              <div
-                className="testimonial"
-                dangerouslySetInnerHTML={{ __html: review.fields.content }}
-              />
-              <div
-                className="author"
-                dangerouslySetInnerHTML={{ __html: review.fields.author }}
-              />
-            </div>
-          );
-        })}
+class WorkshopList extends Component {
+  state = {
+    showAll: false
+  };
+
+  render() {
+    return (
+      <div className="testimonials">
+        <div className="header">Testimonials</div>
+        <div className="items">
+          {_.map(this.props.reviews, (review, index) => {
+            if (index > 2 && !this.state.showAll) return;
+
+            return (
+              <div key={index} className="item">
+                {review.fields.authorPicture && (
+                  <img
+                    src={review.fields.authorPicture}
+                    alt=""
+                    className="author-picture"
+                  />
+                )}
+                <div
+                  className="testimonial"
+                  dangerouslySetInnerHTML={{ __html: review.fields.content }}
+                />
+                <div
+                  className="author"
+                  dangerouslySetInnerHTML={{ __html: review.fields.author }}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="button">
+          <button
+            onClick={() => this.setState({ showAll: !this.state.showAll })}
+          >
+            {this.state.showAll ? '- View Less' : '+ View More'}
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default WorkshopList;
